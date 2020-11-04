371f124afb81a740a7110a57ef084c9dba7acfb1
Ahmed <aa2760@bath.ac.uk>
2020-11-04 01:34:55 +0000

    Adding drill sprite class

---
+++ utils/drill.py
@@ -0,0 +1,28 @@
+"""
+
+"""
+
+import arcade
+class Drill(arcade.Sprite):
+
+    def __init__(self, spriteImage, drillSpeed=2, scale=1, startPositionX=64, startPositionY=128):
+        super().__init__()
+        self.spriteImage = spriteImage
+        self.drillSpeed = drillSpeed
+        self.scale = scale
+        self.center_x = startPositionX
+        self.center_y = startPositionY
+
+        self.texture = arcade.load_texture_pair(spriteImage)[0]
+        self.hit_box = self.texture.hit_box_points
+
+    def setup(self, arcade):
+
+        return arcade.Sprite(self.spriteImage, self.scale)
+
+
+    def changeDrillSpeed(self, newSpeed):
+        """
+
+        """
+        self.drillSpeed = newSpeed
