84ba812f8ca35fb721c3e88b685ed6814c7619ee
Max Mekiska <maxmekiska@gmail.com>
2020-11-24 15:18:44 +0000

    samll correction

--- DrillDungeonGame/entity/entities/green_alien_enemy.py
+++ DrillDungeonGame/entity/entities/green_alien_enemy.py
@@ -14,14 +14,8 @@ class GreenAlienEnemy(Enemy, PathFindingMixin):
     """
     def __init__(self, center_x: int, center_y: int, vision: Union[float, int],
                  speed: Union[float, int] = 1) -> None:
-        base_sprite: str = "resources/images/enemy/enemy.png"
-        sprite_scale: float = 0.3
-        turret_sprite = "resources/images/weapons/turret1.png"
-        turret_sprite_scale = 0.3
-        super().__init__(base_sprite, sprite_scale, center_x, center_y, speed)
-        PathFindingMixin.__init__(self, vision)
         """
-        
+
         Parameters
         ----------
         center_x    :   int
@@ -32,5 +26,12 @@ class GreenAlienEnemy(Enemy, PathFindingMixin):
             The vision/detection field of the enemy.
         speed       :   Union[float, int]
             The movement speed of the enemy.
-        
+
         """
+        base_sprite: str = "resources/images/enemy/enemy.png"
+        sprite_scale: float = 0.3
+        turret_sprite = "resources/images/weapons/turret1.png"
+        turret_sprite_scale = 0.3
+        super().__init__(base_sprite, sprite_scale, center_x, center_y, speed)
+        PathFindingMixin.__init__(self, vision)
+
