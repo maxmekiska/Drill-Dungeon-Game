84e9e1fccef470c2a3fbca2380edb9a73fad5225
Max Mekiska <maxmekiska@gmail.com>
2020-11-24 15:54:23 +0000

    added docstrings

--- DrillDungeonGame/entity/entity.py
+++ DrillDungeonGame/entity/entity.py
@@ -52,19 +52,19 @@ class Entity(arcade.Sprite):
 
         Parameters
         ----------
-        base_sprite: str
+        base_sprite     :   str
             The path to the file containing the sprite for this entity.
-        sprite_scale: float
+        sprite_scale    :   float
             The scale to draw the sprite for this entity
-        center_x: Union[float, int]
+        center_x        :   Union[float, int]
             The starting x position in the map for this entity.
-        center_y: Union[float, int]
+        center_y        :   Union[float, int]
             The starting y position in the map for this entity.
-        speed: Union[float, int]
+        speed           :   Union[float, int]
             The speed that entity can move at.
-        angle: float
+        angle           :   float
             The starting angle for this entity.
-        health: float:
+        health          :   float
             The starting health for this entity.
 
         Returns
@@ -392,6 +392,35 @@ class ChildEntity(Entity):
                  relative_x: Union[float, int] = 0.0, relative_y: Union[float, int] = 0.0,
                  maintain_relative_position: bool = True, speed: float = 1.0,
                  angle: float = 0.0, maintain_parent_angle: bool = True, health: Union[float, int] = -1) -> None:
+        """
+
+        Parameters
+        ----------
+        base_sprite: str
+            The path to the file containing the sprite for this entity.
+        sprite_scale: float
+            The scale to draw the sprite for this entity
+        parent: Entity
+            The entity.
+        relative_x: Union[float, int]
+            The starting x position in the map for this entity.
+        relative_y: Union[float, int]
+            The starting y position in the map for this entity.
+        maintain_relative_position: bool
+        speed: Union[float, int]
+            The speed that entity can move at.
+        angle: float
+            The starting angle for this entity.
+        maintain_parent_angle
+            Angle of parent.
+        health: Union[float, int]:
+            The starting health for this entity.
+
+        Returns
+        -------
+        None
+
+        """
         super().__init__(base_sprite=base_sprite, sprite_scale=sprite_scale, center_x=parent.center_x + relative_x,
                          center_y=parent.center_y + relative_y, angle=angle, speed=speed, health=health)
         self.parent = parent
--- DrillDungeonGame/map/chunk.py
+++ DrillDungeonGame/map/chunk.py
@@ -3,14 +3,54 @@ from DrillDungeonGame.sprite_container import SpriteContainer
 
 
 class Chunk:
+    """
+
+    Class to populate map with different chunks.
+
+    Methods
+    -------
+    load_chunk_sprites()
+        Identifies different chunks in the chunk_matrix and populates the map with the appropriate sprite.
+    get_chunk_center()
+        Returns x, y coordinates of chunk center.
+
+    """
 
     def __init__(self, chunk_matrix: list):
+        """
+
+        Parameters
+        ----------
+        chunk_matrix: List(chunk_matrix)
+            Randomly created list containing different symbols (X, C, G, O) to determine appropriate sprite allocation.
+
+        """
         self.chunk_matrix = chunk_matrix
         self.chunk_sprites = SpriteContainer(None, arcade.SpriteList(), arcade.SpriteList(), arcade.SpriteList(), arcade.SpriteList(), arcade.SpriteList(), arcade.SpriteList(), arcade.SpriteList(), arcade.SpriteList(), arcade.SpriteList(), arcade.SpriteList(), arcade.SpriteList(), arcade.SpriteList(), arcade.SpriteList()) 
         self.load_chunk_sprites()
         self.chunk_center = self.get_chunk_center()
 
     def load_chunk_sprites(self):
+        """
+        Identifies and loads different chunks given by the chunk_matrix and populates the game map with the
+        appropriate sprites.
+
+        Note
+        ----
+        X -> Dirt block
+        C -> Coal block
+        G -> Gold block
+        O -> Border block
+
+        Parameters
+        ----------
+        None
+
+        Returns
+        -------
+        None
+
+        """
         for row in self.chunk_matrix:
             for item in row:
                 if item[0] == 'X':  # Dirt
@@ -45,7 +85,17 @@ class Chunk:
 
     def get_chunk_center(self):
         """
-        Returns the x, y coordinates of the center of the chunk
+
+        Returns the x, y coordinates of the center of the chunk.
+
+        Parameters
+        ----------
+        None
+
+        Returns
+        -------
+        None
+
         """
         startX, startY = self.chunk_matrix[0][0][1], self.chunk_matrix[0][0][2]
         endX, endY = self.chunk_matrix[-1][-1][1], self.chunk_matrix[-1][-1][2]
